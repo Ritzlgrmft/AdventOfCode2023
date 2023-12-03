e2dd5608e2a71da98984bee453ae0cc0d4178f78
Markus Wagner <markus.wagner02@web.de>
2023-12-03 09:37:27 +0100

    add day 3

---
+++ Day3a/Worker.cs
@@ -0,0 +1,84 @@
+﻿using System.Dynamic;
+
+namespace AdventOfCode2023.Day3a;
+
+public class Worker : IWorker
+{
+    public int DoWork(string inputFile)
+    {
+        var numbers = new List<Number>();
+        var symbols = new List<Symbol>();
+        var row = 0;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var col = 0;
+            var isNumberParsing = false;
+            var currentNumber = 0;
+            var startCol = 0;
+            foreach (var c in line.ToCharArray())
+            {
+                if (char.IsDigit(c))
+                {
+                    if (!isNumberParsing)
+                    {
+                        startCol = col;
+                        currentNumber = c - '0';
+                    }
+                    else
+                    {
+                        currentNumber = currentNumber * 10 + c - '0';
+                    }
+                    isNumberParsing = true;
+                }
+                else
+                {
+                    if (isNumberParsing)
+                    {
+                        numbers.Add(new Number { row = row, col = startCol, length = col - startCol, value = currentNumber });
+                        isNumberParsing = false;
+                    }
+
+                    if (c != '.')
+                    {
+                        symbols.Add(new Symbol { row = row, col = col, value = c });
+                    }
+                }
+
+                col++;
+            }
+
+            if (isNumberParsing)
+            {
+                numbers.Add(new Number { row = row, col = startCol, length = col - startCol, value = currentNumber });
+                isNumberParsing = false;
+            }
+
+            row++;
+        }
+
+        var sum = 0;
+        foreach (var number in numbers)
+        {
+            if (symbols.Any(s => Enumerable.Range(number.row - 1, 3).Contains(s.row) && Enumerable.Range(number.col - 1, number.length + 2).Contains(s.col)))
+            {
+                sum += number.value;
+            }
+        }
+        return sum;
+    }
+}
+
+struct Number
+{
+    public int row;
+    public int col;
+    public int length;
+    public int value;
+}
+
+struct Symbol
+{
+    public int row;
+    public int col;
+    public char value;
+}
---
+++ Day3b/Worker.cs
@@ -0,0 +1,85 @@
+﻿using System.Dynamic;
+
+namespace AdventOfCode2023.Day3b;
+
+public class Worker : IWorker
+{
+    public int DoWork(string inputFile)
+    {
+        var numbers = new List<Number>();
+        var symbols = new List<Symbol>();
+        var row = 0;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var col = 0;
+            var isNumberParsing = false;
+            var currentNumber = 0;
+            var startCol = 0;
+            foreach (var c in line.ToCharArray())
+            {
+                if (char.IsDigit(c))
+                {
+                    if (!isNumberParsing)
+                    {
+                        startCol = col;
+                        currentNumber = c - '0';
+                    }
+                    else
+                    {
+                        currentNumber = currentNumber * 10 + c - '0';
+                    }
+                    isNumberParsing = true;
+                }
+                else
+                {
+                    if (isNumberParsing)
+                    {
+                        numbers.Add(new Number { row = row, col = startCol, length = col - startCol, value = currentNumber });
+                        isNumberParsing = false;
+                    }
+
+                    if (c != '.')
+                    {
+                        symbols.Add(new Symbol { row = row, col = col, value = c });
+                    }
+                }
+
+                col++;
+            }
+
+            if (isNumberParsing)
+            {
+                numbers.Add(new Number { row = row, col = startCol, length = col - startCol, value = currentNumber });
+                isNumberParsing = false;
+            }
+
+            row++;
+        }
+
+        var sum = 0;
+        foreach (var symbol in symbols.Where(s => s.value == '*'))
+        {
+            var adjacentNumbers = numbers.Where(n => Enumerable.Range(n.row - 1, 3).Contains(symbol.row) && Enumerable.Range(n.col - 1, n.length + 2).Contains(symbol.col));
+            if (adjacentNumbers.Count() == 2)
+            {
+                sum += adjacentNumbers.First().value * adjacentNumbers.Last().value;
+            }
+        }
+        return sum;
+    }
+}
+
+struct Number
+{
+    public int row;
+    public int col;
+    public int length;
+    public int value;
+}
+
+struct Symbol
+{
+    public int row;
+    public int col;
+    public char value;
+}
