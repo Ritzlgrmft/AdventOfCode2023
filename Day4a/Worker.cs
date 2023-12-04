61383bec3cc41d1a0cbd026f66f5bdb1831b7584
Markus Wagner <markus.wagner02@web.de>
2023-12-04 19:12:08 +0100

    add day4

---
+++ Day4a/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System.Dynamic;
+
+namespace AdventOfCode2023.Day4a;
+
+public class Worker : IWorker
+{
+    public int DoWork(string inputFile)
+    {
+        var cards = new List<Card>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var card = new Card();
+            var lineParts = line.Split(new[] { ':', '|' }, StringSplitOptions.RemoveEmptyEntries);
+            card.winningNumbers = lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+            card.numbersYouHave = lineParts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+            cards.Add(card);
+        }
+
+        var sum = 0;
+        foreach (var card in cards)
+        {
+            var winners = card.winningNumbers.Intersect(card.numbersYouHave).ToList();
+            if (winners.Any())
+            {
+                sum += Enumerable.Repeat(2, winners.Count() - 1).Aggregate(1, (a, b) => a * b);
+            }
+        }
+        return sum;
+    }
+}
+
+struct Card
+{
+    public List<int> winningNumbers;
+    public List<int> numbersYouHave;
+}
---
+++ Day4b/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System.Dynamic;
+
+namespace AdventOfCode2023.Day4b;
+
+public class Worker : IWorker
+{
+    public int DoWork(string inputFile)
+    {
+        var cards = new List<Card>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var lineParts = line.Split(new[] { ':', '|' }, StringSplitOptions.RemoveEmptyEntries);
+            var card = new Card(
+                lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList(),
+                lineParts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList());
+            cards.Add(card);
+        }
+
+        for (var cardIndex = 0; cardIndex < cards.Count; cardIndex++)
+        {
+            var card = cards[cardIndex];
+            var winners = card.winningNumbers.Intersect(card.numbersYouHave).ToList();
+            for (var copyIndex = 0; copyIndex < winners.Count; copyIndex++)
+            {
+                var copyCard = cards[cardIndex + copyIndex + 1];
+                copyCard.numberOfCard += card.numberOfCard;
+            }
+        }
+        return cards.Sum(c => c.numberOfCard);
+    }
+}
+
+record Card
+{
+    public List<int> winningNumbers;
+    public List<int> numbersYouHave;
+    public int numberOfCard;
+
+    public Card(List<int> winningNumbers, List<int> numbersYouHave)
+    {
+        this.winningNumbers = winningNumbers;
+        this.numbersYouHave = numbersYouHave;
+        this.numberOfCard = 1;
+    }
+}
