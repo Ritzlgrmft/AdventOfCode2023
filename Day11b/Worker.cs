4d6f1a79649ad85296f9979bfbd51cf3603a1d2f
Markus Wagner <markus.wagner02@web.de>
2023-12-11 18:16:22 +0100

    add day 11

---
+++ Day11b/Worker.cs
@@ -0,0 +1,74 @@
+﻿using System.Data;
+
+namespace AdventOfCode2023.Day11b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        // read map
+        var map = new List<List<char>>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var row = line.ToCharArray().ToList();
+            map.Add(row);
+        }
+
+        // find empty rows
+        var emptyRows = new List<int>();
+        for (var row = 0; row < map.Count; row++)
+        {
+            if (map[row].All(c => c == '.'))
+            {
+                emptyRows.Add(row);
+            }
+        }
+
+        // find empty cols
+        var emptyCols = new List<int>();
+        for (var col = 0; col < map[0].Count; col++)
+        {
+            if (map.Select(row => row[col]).All(c => c == '.'))
+            {
+                emptyCols.Add(col);
+            }
+        }
+
+        // find galaxies
+        var galaxies = new List<(int Row, int Col)>();
+        for (var row = 0; row < map.Count; row++)
+        {
+            for (var col = 0; col < map[0].Count; col++)
+            {
+                if (map[row][col] == '#')
+                {
+                    galaxies.Add((row, col));
+                }
+            }
+        }
+
+        // calculate paths
+        var sum = 0L;
+        for (var a = 0; a < galaxies.Count; a++)
+        {
+            var galaxyA = galaxies[a];
+            for (var b = a + 1; b < galaxies.Count; b++)
+            {
+                var galaxyB = galaxies[b];
+                var minRow = Math.Min(galaxyA.Row, galaxyB.Row);
+                var maxRow = Math.Max(galaxyA.Row, galaxyB.Row);
+                var minCol = Math.Min(galaxyA.Col, galaxyB.Col);
+                var maxCol = Math.Max(galaxyA.Col, galaxyB.Col);
+                var factor = 999999;
+                var emptyRowsInBetween = emptyRows.Count(r => minRow < r && r < maxRow);
+                var emptyColsInBetween = emptyCols.Count(c => minCol < c && c < maxCol);
+                var dist = maxRow - minRow + emptyRowsInBetween * factor + maxCol - minCol + emptyColsInBetween * factor;
+                sum += dist;
+            }
+
+        }
+
+        return sum;
+    }
+
+}
