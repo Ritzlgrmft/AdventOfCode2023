6f5bf9b5406045cc91f68d314265391f0a224a9c
Markus Wagner <markus.wagner02@web.de>
2023-12-12 23:01:27 +0100

    add day 12

---
+++ Day12a/Worker.cs
@@ -0,0 +1,96 @@
+﻿using System.Data;
+
+namespace AdventOfCode2023.Day12a;
+
+public class Worker : IWorker
+{
+
+    public long DoWork(string inputFile)
+    {
+
+        var springMap = new List<(char[] Springs, int[] ContigousGroupLengths)>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var lineParts = line.Split(" ");
+            springMap.Add((lineParts[0].ToCharArray(), lineParts[1].Split(",").Select(n => int.Parse(n)).ToArray()));
+        }
+
+        var sum = 0L;
+        foreach (var springEntry in springMap)
+        {
+            var potentialSprings = GetPotentialSprings(springEntry.Springs);
+            foreach (var potentialSpring in potentialSprings)
+            {
+                if (AreLengthsEqual(GetContigousLengths(potentialSpring), springEntry.ContigousGroupLengths))
+                {
+                    sum++;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private int[] GetContigousLengths(char[] springs)
+    {
+        return springs.GroupWhile((a, b) => a == b).Where(g => g.First() == '#').Select(g => g.Count()).ToArray();
+    }
+
+    private bool AreLengthsEqual(int[] a, int[] b)
+    {
+        if (a.Length != b.Length)
+        {
+            return false;
+        }
+        for (var i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<char[]> GetPotentialSprings(char[] input)
+    {
+        if (!input.Any(c => c == '?'))
+        {
+            return new List<char[]>() { input };
+        }
+
+        var i = 0;
+        while (input[i] != '?')
+        {
+            i++;
+        }
+        var variant1 = input.Select(c => c).ToArray();
+        variant1[i] = '.';
+        var variant2 = input.Select(c => c).ToArray();
+        variant2[i] = '#';
+        return GetPotentialSprings(variant1).Union(GetPotentialSprings(variant2)).ToList();
+    }
+
+}
+
+public static class LinqExtension
+{
+    public static IEnumerable<IEnumerable<T>> GroupWhile<T>(this IEnumerable<T> seq, Func<T, T, bool> condition)
+    {
+        T prev = seq.First();
+        List<T> list = new List<T>() { prev };
+
+        foreach (T item in seq.Skip(1))
+        {
+            if (condition(prev, item) == false)
+            {
+                yield return list;
+                list = new List<T>();
+            }
+            list.Add(item);
+            prev = item;
+        }
+
+        yield return list;
+    }
+}
---
+++ Day12b/Worker.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+
+namespace AdventOfCode2023.Day12b;
+
+public class Worker : IWorker
+{
+
+    Dictionary<string, long> cache = new Dictionary<string, long>();
+
+    public long DoWork(string inputFile)
+    {
+
+        var springMap = new List<(string Springs, int[] ContigousGroupLengths)>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var lineParts = line.Split(" ");
+            var springs = lineParts[0] + "?" + lineParts[0] + "?" + lineParts[0] + "?" + lineParts[0] + "?" + lineParts[0];
+            var lengths = lineParts[1] + "," + lineParts[1] + "," + lineParts[1] + "," + lineParts[1] + "," + lineParts[1];
+            springMap.Add((springs, lengths.Split(",").Select(int.Parse).ToArray()));
+        }
+
+        var sum = 0L;
+        foreach (var springEntry in springMap)
+        {
+            var numberOfMatchingSprings = GetNumberOfMatchingSpringsWithCache(springEntry.Springs, springEntry.ContigousGroupLengths);
+            sum += numberOfMatchingSprings;
+        }
+
+        return sum;
+    }
+
+    private long GetNumberOfMatchingSpringsWithCache(string input, int[] targetLengths)
+    {
+        var cacheKey = input + " " + string.Join(",", targetLengths);
+        if (cache.ContainsKey(cacheKey))
+        {
+            return cache[cacheKey];
+        }
+
+        var value = GetNumberOfMatchingSprings(input, targetLengths);
+        cache[cacheKey] = value;
+        return value;
+    }
+
+    private long GetNumberOfMatchingSprings(string input, int[] targetLengths)
+    {
+        var index = 0;
+        while (index < input.Length && input[index] == '.')
+        {
+            index++;
+        }
+
+        if (index == input.Length)
+        {
+            return targetLengths.Length == 0 ? 1 : 0;
+        }
+
+        var springStart = index;
+        while (index < input.Length && input[index] == '#')
+        {
+            index++;
+        }
+
+        if (index == input.Length)
+        {
+            if (targetLengths.Length == 1 && index - springStart == targetLengths[0])
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        if (input[index] == '.')
+        {
+            if (targetLengths.Length > 0 && index - springStart == targetLengths[0])
+            {
+                return GetNumberOfMatchingSpringsWithCache(input[index..], targetLengths.Skip(1).ToArray());
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            var result = 0L;
+            if (targetLengths.Length > 0 && index - springStart == targetLengths[0])
+            {
+                result += GetNumberOfMatchingSpringsWithCache("." + input[(index + 1)..], targetLengths.Skip(1).ToArray());
+            }
+            else
+            {
+                if (targetLengths.Length > 0 && index - springStart < targetLengths[0])
+                {
+                    result += GetNumberOfMatchingSpringsWithCache(input[..index] + "#" + input[(index + 1)..], targetLengths);
+                }
+                if (index == springStart)
+                {
+                    result += GetNumberOfMatchingSpringsWithCache(input[(index + 1)..], targetLengths);
+                }
+            }
+            return result;
+        }
+
+    }
+}
+
