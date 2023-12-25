6eeebbb0f5e7b81389f5125fc367df0868f02062
Markus Wagner <markus.wagner02@web.de>
2023-12-25 19:36:41 +0100

    add day 18

---
+++ Day18a/Worker.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.Mail;
+using AdventOfCode2023.Day4a;
+
+namespace AdventOfCode2023.Day18a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        // read input
+        var holes = new List<(int Row, int Col, string Color)>();
+        var currentIndex = -1;
+        var currentRow = 0;
+        var currentCol = 0;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var lineParts = line.Split(new char[] { ' ', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            var direction = lineParts[0];
+            var meters = int.Parse(lineParts[1]);
+            var color = lineParts[2];
+
+            for (var i = 0; i < meters; i++)
+            {
+                switch (lineParts[0])
+                {
+                    case "U":
+                        holes.Add((currentRow - 2 * i - 1, currentCol, color));
+                        holes.Add((currentRow - 2 * i - 2, currentCol, color));
+                        break;
+                    case "R":
+                        holes.Add((currentRow, currentCol + 2 * i + 1, color));
+                        holes.Add((currentRow, currentCol + 2 * i + 2, color));
+                        break;
+                    case "D":
+                        holes.Add((currentRow + 2 * i + 1, currentCol, color));
+                        holes.Add((currentRow + 2 * i + 2, currentCol, color));
+                        break;
+                    case "L":
+                        holes.Add((currentRow, currentCol - 2 * i - 1, color));
+                        holes.Add((currentRow, currentCol - 2 * i - 2, color));
+                        break;
+                }
+            }
+            currentIndex += 2 * meters;
+            currentRow = holes[currentIndex].Row;
+            currentCol = holes[currentIndex].Col;
+        }
+        Console.WriteLine($"Rows: {holes.Select(h => h.Row).Min()}-{holes.Select(h => h.Row).Max()}, Cols: {holes.Select(h => h.Col).Min()}-{holes.Select(h => h.Col).Max()}");
+
+        // build map
+        var minRow = holes.Select(h => h.Row).Min();
+        var maxRow = holes.Select(h => h.Row).Max();
+        var minCol = holes.Select(h => h.Col).Min();
+        var maxCol = holes.Select(h => h.Col).Max();
+        var map = new (bool Hole, string Color)[maxRow - minRow + 1, maxCol - minCol + 1];
+        var x = map.GetLength(0);
+        var y = map.GetLength(1);
+        for (var row = minRow; row <= maxRow; row++)
+        {
+            for (var col = minCol; col <= maxCol; col++)
+            {
+                if (holes.Any(h => h.Row == row && h.Col == col))
+                {
+                    map[row - minRow, col - minCol] = (true, holes.FirstOrDefault(h => h.Row == row && h.Col == col).Color);
+                }
+            }
+        }
+
+        // fill interior
+        var floodStack = new List<(int Row, int Col)>();
+        for (var row = 1; row < map.GetLength(0) && floodStack.Count == 0; row++)
+        {
+            var col = 0;
+            while (!map[row, col].Hole)
+            {
+                col++;
+            }
+            if (!map[row, col + 1].Hole)
+            {
+                floodStack.Add((row, col + 1));
+            }
+        }
+        var iteration = 0L;
+        while (floodStack.Count > 0)
+        {
+            var current = floodStack.Last();
+            floodStack.Remove(current);
+
+            map[current.Row, current.Col] = (true, "");
+
+            if (!map[current.Row, current.Col - 1].Hole && !floodStack.Any(f => f.Row == current.Row && f.Col == current.Col - 1))
+            {
+                floodStack.Add((current.Row, current.Col - 1));
+            }
+            if (!map[current.Row, current.Col + 1].Hole && !floodStack.Any(f => f.Row == current.Row && f.Col == current.Col + 1))
+            {
+                floodStack.Add((current.Row, current.Col + 1));
+            }
+            if (!map[current.Row - 1, current.Col].Hole && !floodStack.Any(f => f.Row == current.Row - 1 && f.Col == current.Col))
+            {
+                floodStack.Add((current.Row - 1, current.Col));
+            }
+            if (!map[current.Row + 1, current.Col].Hole && !floodStack.Any(f => f.Row == current.Row + 1 && f.Col == current.Col))
+            {
+                floodStack.Add((current.Row + 1, current.Col));
+            }
+            iteration++;
+
+            if (iteration % 1000 == 0)
+            {
+                Console.WriteLine($"{iteration}: {floodStack.Count} stack size");
+            }
+        }
+
+        var sum = 0L;
+        for (var row = 0; row < map.GetLength(0); row += 2)
+        {
+            for (var col = 0; col < map.GetLength(1); col += 2)
+            {
+                if (map[row, col].Hole)
+                {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+}
---
+++ Day18b/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Pipes;
+using System.Net.Mail;
+using AdventOfCode2023.Day4a;
+
+namespace AdventOfCode2023.Day18b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var corners = new List<(long Row, long Col)>() { (0L, 0L) };
+        var currentRow = 0L;
+        var currentCol = 0L;
+        var perimeter = 0L;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var lineParts = line.Split(new char[] { ' ', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            var color = lineParts[2];
+            var meters = Convert.ToInt64(color.Substring(1, 5), 16);
+            var direction = color.Substring(6);
+
+            switch (direction)
+            {
+                case "0":
+                    // right
+                    currentCol += meters;
+                    break;
+                case "1":
+                    // down
+                    currentRow += meters;
+                    break;
+                case "2":
+                    // left
+                    currentCol -= meters;
+                    break;
+                case "3":
+                    // up
+                    currentRow -= meters;
+                    break;
+            }
+
+            perimeter += meters;
+            corners.Add((currentRow, currentCol));
+        }
+
+        var area = Math.Abs(corners.Take(corners.Count - 1)
+            .Select((p, i) => (corners[i + 1].Col - p.Col) * (corners[i + 1].Row + p.Row))
+            .Sum() / 2);
+
+        return area + perimeter / 2 + 1;
+    }
+
+}
